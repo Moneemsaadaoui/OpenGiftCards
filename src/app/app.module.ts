ac4ebe6aa3f20534839e00d1b3ce15bf4c9235f1
Abdel Moneem Saadaoui <contact.moneem@gmail.com>
2022-10-06 21:32:36 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CardComponentComponent } from './card-component/card-component.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponentComponent,
+    SearchbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
